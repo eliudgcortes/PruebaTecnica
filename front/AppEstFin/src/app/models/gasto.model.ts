7e2d60e80a65155af7ef44e93b0e1b6c817411d8
eliudgcortes <43182648+eliudgcortes@users.noreply.github.com>
2026-09-23 23:29:02 -0600

    Update gasto.model.ts
    Se agregaron comentarios descriptivos a la interface de Gasto.

--- front/AppEstFin/src/app/models/gasto.model.ts
+++ front/AppEstFin/src/app/models/gasto.model.ts
@@ -1,7 +1,17 @@
-export interface Gasto {
+export interface Gasto 
+{
+    // ID del gasto
     id: number;
+
+    // Monto del gasto
     monto: number;
+
+    // Descripcion del gasto
     descripcion: string;
+
+    // Categoria del gasto
     categoriaGasto: string;
+
+    // Fecha del movimiento
     fechaMovimiento: string;
 }
