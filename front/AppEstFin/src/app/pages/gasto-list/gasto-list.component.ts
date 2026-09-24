e9dbccb3a8805de468ee1a63aa1e8da63d4016f6
eliudgcortes <43182648+eliudgcortes@users.noreply.github.com>
2026-09-23 23:34:08 -0600

    Update gasto-list.component.ts
    Agregando comentarios descriptivos para entender el flujo de la clase, array/arreglo, constructor, ngOnInit y id detalle de gasto.

--- front/AppEstFin/src/app/pages/gasto-list/gasto-list.component.ts
+++ front/AppEstFin/src/app/pages/gasto-list/gasto-list.component.ts
@@ -6,22 +6,22 @@ import { GastoService } from '../../servicios/gasto.service';
   standalone: true,
   templateUrl: './gasto-list.component.html'
 })
-
 export class GastoListComponent {
+
+  // Lista de gastos.
   gastos: any[] = [];
 
   constructor(
     private gastoService: GastoService
   ) {}
 
-
+  // Se ejecuta al iniciar.
   ngOnInit(): void {
     this.gastoService.obtenerGastos();
   }
 
-
+  // Muestra el detalle del gasto.
   verDetalle(id: number) {
 
   }
-
 }
